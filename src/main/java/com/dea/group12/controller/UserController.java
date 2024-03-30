d68a559a968457092c84e28f57c956854f64c634
chathura456 <chathuracde@gmail.com>
2024-03-30 21:09:59 +0530

    name change controller

--- src/main/java/com/dea/group12/controller/UserController.java
+++ src/main/java/com/dea/group12/controller/UserController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-public class MainController {
+public class UserController {
     @Autowired
     private UserService userService;
 
