5606284326e25092620aca7e5fe12aec3abbe5f9
sahas <sahasra.weerasinghe99@gmail.com>
2024-04-26 15:35:59 +0530

    Add Supplier Code Section

---
+++ src/main/java/com/dea/group12/repository/SupplierRepository.java
@@ -0,0 +1,11 @@
+//SupplierRepository
+
+package com.dea.group12.repository;
+
+import com.dea.group12.model.Supplier;
+import com.dea.group12.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
+    Supplier findByEmail(String email);
+}
