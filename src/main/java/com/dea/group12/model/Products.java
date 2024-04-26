974993bce562229aa037e5f2ac2d7848479938cf
Chathura Dilshan <60825886+chathura456@users.noreply.github.com>
2024-04-27 03:19:08 +0530

    Revert "Products"

--- src/main/java/com/dea/group12/model/Products.java
+++ src/main/java/com/dea/group12/model/Products.java
@@ -1,4 +1,50 @@
+//Products
 package com.dea.group12.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import javax.annotation.processing.Generated;
+
+@Entity
 public class Products {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int id;
+    private String pname;
+    private double pamount;
+    private int quantity;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    public double getPamount() {
+        return pamount;
+    }
+
+    public void setPamount(double pamount) {
+        this.pamount = pamount;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
