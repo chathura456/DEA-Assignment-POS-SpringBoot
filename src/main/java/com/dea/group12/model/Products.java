ee5209d8148d61bbd9a2386a030745afa0a5f7e4
Viraj-Tharaka <virajtharaka97@gmail.com>
2024-04-27 08:51:34 +0530

    Products model, repository and service updated.

--- src/main/java/com/dea/group12/model/Products.java
+++ src/main/java/com/dea/group12/model/Products.java
@@ -1,4 +1,50 @@
+//Products
 package com.dea.group12.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import javax.annotation.processing.Generated;
+
+@Entity
 public class Products {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int pid;
+    private String pname;
+    private double pamount;
+    private int quantity;
+    public int getId() {
+        return pid;
+    }
+
+    public void setId(int pid) {
+        this.pid = pid;
+    }
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    public double getPamount() {
+        return pamount;
+    }
+
+    public void setPamount(double pamount) {
+        this.pamount = pamount;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
